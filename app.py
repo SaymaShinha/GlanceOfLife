222de814c79252fb401260a0dedeee1f3234e134
SaymaShinha <sayma.shinha@gmail.com>
2025-03-06 20:48:52 +0600

    single project page first commit

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return render_template('index.html')
+  return render_template('home.html')
+
+@app.route('/project/<string:project_name>')
+def project(project_name):
+   
+   return render_template('project.html', project_name=project_name)
 
 if __name__ == '__main__':
    app.run(debug = True, port=5000)
